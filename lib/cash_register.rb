f652203ad08fbb85c707414529e351f34f5d8e16
Dana Palmer <roiai4@hotmail.com>
2018-09-24 05:04:40 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,31 +1,36 @@
+require 'pry'
 class CashRegister
-
-  attr_accessor :items, :discount, :total, :last_transaction
-
-  def initialize(discount=0)
-    @total = 0
+  
+    attr_accessor :cart, :discount, :total, :last_transaction
+#last_transactiion is used for what now?
+  
+  def initialize(discount = 0)
+    @total = 0 
     @discount = discount
-    @items = []
+    @cart = []
   end
 
-  def add_item(title, amount, quantity=1)
-    self.total += amount * quantity
-    quantity.times do
-      items << title
-    end
-    self.last_transaction = amount * quantity
+def add_item(item_name, price, quantity=1)
+  self.total += quantity * price
+  quantity.times do
+    @cart << item_name
   end
+  self.last_transaction = price * quantity
+end
 
-  def apply_discount
-    if discount != 0
-      self.total = (total * ((100.0 - discount.to_f)/100)).to_i
-      "After the discount, the total comes to $#{self.total}."
-    else
-      "There is no discount to apply."
-    end
-  end
-
-  def void_last_transaction
-    self.total = self.total - self.last_transaction
+def apply_discount
+  if discount != 0 
+    self.total = (total * (100.0 - discount.to_f)/100).to_i
+    "After the discount, the total comes to $#{self.total}."
+  else
+    "There is no discount to apply."
   end
 end
+
+def void_last_transaction
+  self.total = self.total - self.last_transaction
+end
+
+
+end
+
